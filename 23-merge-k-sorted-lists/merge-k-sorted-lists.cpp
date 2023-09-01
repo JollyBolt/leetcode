00a2a5ebde4044e89fee0bd593ca2208691a5bf6
Ishan <senishan21@gmail.com>
2023-09-01 15:00:27 +0530

    Time: 14 ms (94.40%) | Memory: 13.7 MB (31.87%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -10,25 +10,72 @@
  */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<int,vector<int>,greater<int>> minh;
-        int n=lists.size();
-        for(int i=0;i<n;i++){
-            ListNode *temp = lists[i];
-            while(temp){
-                minh.push(temp->val);
-                temp=temp->next;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *head,*temp;
+        head = new ListNode();
+        temp=head;
+        while(list1 and list2)
+        {
+            if(list1->val<list2->val)
+            {
+                temp->next=list1;
+                list1=list1->next;
             }
+            else
+            {
+                temp->next=list2;
+                list2=list2->next;
+            }
+            temp=temp->next;           
         }
-        ListNode *head = new ListNode();
-        ListNode *temp=head;
-        while(!minh.empty()){
-            int x=minh.top();
-            ListNode *dummy = new ListNode(x);
-            temp->next=dummy;
-            temp=temp->next;
-            minh.pop();
+        if(list1)
+        {
+            temp->next=list1;
+        }
+        if(list2)
+        {
+            temp->next=list2;
         }
         return head->next;
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        //Using Heap
+        // priority_queue<int,vector<int>,greater<int>> minh;
+        // int n=lists.size();
+        // for(int i=0;i<n;i++){
+        //     ListNode *temp = lists[i];
+        //     while(temp){
+        //         minh.push(temp->val);
+        //         temp=temp->next;
+        //     }
+        // }
+        // ListNode *head = new ListNode();
+        // ListNode *temp=head;
+        // while(!minh.empty()){
+        //     int x=minh.top();
+        //     ListNode *dummy = new ListNode(x);
+        //     temp->next=dummy;
+        //     temp=temp->next;
+        //     minh.pop();
+        // }
+        // return head->next;
+
+        //Using Merge
+        int n=lists.size();
+        if(n==0) return NULL;
+        cout<<n;
+        while(lists.size()>1){
+            n=lists.size();
+            vector<ListNode*> v;    
+            for(int i=0;i<n;i+=2){
+                ListNode *one = lists[i],*two=(i>=n-1)?NULL:lists[i+1];
+                v.push_back(mergeTwoLists(one,two));
+            }
+            lists=v;
+            cout<<lists.size();
+        }
+        return lists[0];
+        
     }
 };
