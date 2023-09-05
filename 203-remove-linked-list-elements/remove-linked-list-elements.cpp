b0ffc94598d28a4dbd7a4bc3d81d244c28dad775
Ishan <senishan21@gmail.com>
2023-09-06 01:51:11 +0530

    Time: 14 ms (93.16%) | Memory: 15.5 MB (7.50%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.cpp
+++ 203-remove-linked-list-elements/remove-linked-list-elements.cpp
@@ -11,17 +11,25 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        ListNode *shead=new ListNode(0,head);
-        ListNode* temp=shead;
-        while(head)
-        {   
-            while(head and head->val==val) head=head->next;
+        //Iterative
+    //     ListNode *shead=new ListNode(0,head);
+    //     ListNode* temp=shead;
+    //     while(head)
+    //     {   
+    //         while(head and head->val==val) head=head->next;
 
-            temp->next=head;
-            temp=head;
-            if(head) head=head->next; 
-        }
+    //         temp->next=head;
+    //         temp=head;
+    //         if(head) head=head->next; 
+    //     }
 
-    return shead->next;    
+    // return shead->next; 
+
+        //Recursive
+        if(!head) return NULL;
+        ListNode *temp=removeElements(head->next,val);
+        if(head->val==val) return temp;
+        head->next=temp;
+        return head;   
     }
 };
