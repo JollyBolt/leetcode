accdda3be29dcb4a0ea97f4ee8d8bb36a12266bb
Ishan <senishan21@gmail.com>
2023-09-05 21:37:46 +0530

    Time: 40 ms (62.94%) | Memory: 14.7 MB (33.85%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,34 +9,46 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int c1=0,c2=0;
-        ListNode *temp=headA;
-        while(temp){
-            c1++;
-            temp=temp->next;
+        //Initial Approach
+        // int c1=0,c2=0;
+        // ListNode *temp=headA;
+        // while(temp){
+        //     c1++;
+        //     temp=temp->next;
+        // }
+        // temp=headB;
+        // while(temp){
+        //     c2++;
+        //     temp=temp->next;
+        // }
+        // if(c1>c2){
+        //     int x=c1-c2;
+        //     while(x--) headA=headA->next;
+        // }
+        // else {
+        //     int x=c2-c1;
+        //     while(x--) headB=headB->next;
+        // }
+        // while(headA){
+        //     if(headA==headB)
+        //     return headA;
+        //     else {
+        //         headA=headA->next;
+        //         headB=headB->next; 
+        //     }
+        // }
+        // return NULL;
+
+        //Different Approach
+        ListNode* a=headA,*b=headB;
+        while(a!=b){
+            if(!a) a=headB;
+            else a=a->next;
+
+            if(!b) b=headA;
+            else b=b->next;
         }
-        temp=headB;
-        while(temp){
-            c2++;
-            temp=temp->next;
-        }
-        if(c1>c2){
-            int x=c1-c2;
-            while(x--) headA=headA->next;
-        }
-        else {
-            int x=c2-c1;
-            while(x--) headB=headB->next;
-        }
-        while(headA){
-            if(headA==headB)
-            return headA;
-            else {
-                headA=headA->next;
-                headB=headB->next; 
-            }
-        }
-        return NULL;
+        return a;
 
     }
 };
