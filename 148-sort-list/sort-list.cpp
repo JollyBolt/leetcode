ed90df43506b513567b42744cf5dc04ac1d44bc2
Ishan <senishan21@gmail.com>
2023-09-07 17:29:08 +0530

    Time: 180 ms (55.90%) | Memory: 74.2 MB (8.31%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,7 +10,52 @@
  */
 class Solution {
 public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *head,*temp;
+        head = new ListNode();
+        temp=head;
+        while(list1 and list2)
+        {
+            if(list1->val<list2->val)
+            {
+                temp->next=list1;
+                list1=list1->next;
+            }
+            else
+            {
+                temp->next=list2;
+                list2=list2->next;
+            }
+            temp=temp->next;           
+        }
+        if(list1)
+        {
+            temp->next=list1;
+        }
+        if(list2)
+        {
+            temp->next=list2;
+        }
+        return head->next;
+    }
+
+    ListNode* mergeSort(ListNode* head){
+        if(!head or !head->next) return head;
+
+        ListNode* fast=head,*slow=head;
+        while(fast->next and fast->next->next){
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        ListNode* newNode = slow->next;
+        slow->next=NULL;
+        ListNode* list1=mergeSort(head);
+        ListNode* list2=mergeSort(newNode);
+        return mergeTwoLists(list1,list2);
+    }
     ListNode* sortList(ListNode* head) {
+        //Simple Recursive Approach
+        /*
         if(!head or !head->next) return head;
         ListNode *newnode = sortList(head->next);
         ListNode*temp=head;
@@ -19,5 +64,10 @@ public:
             temp=temp->next;
         }
         return head;
+        Time Complexity: O(n^2)
+        Space Complexity: O(n) for recursive stack
+        */
+        //Merge sort Approach
+        return mergeSort(head);
     }
 };
