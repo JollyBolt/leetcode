38c3b662f3f5bd8dd76f355e4ab6598a6883dc4b
Ishan <senishan21@gmail.com>
2023-09-13 09:47:07 +0530

    Time: 37 ms (97.66%) | Memory: 17.3 MB (58.53%) - LeetSync

--- 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -7,27 +7,14 @@ public:
             v[s[i] -'a']++;
         }
         sort(v.begin(),v.end());
-        int index=0;
-        while(v[index]==0) index++;
-        unordered_map<int,int>m;
-        for(int i=index;i<26;i++){
-            m[v[i]]++;
-        }
+
         int ans=0;
-        int x=v[25]-1;
-        cout<<x;
-        for(int i=25;i>index;i--){
-            if(v[i]==v[i-1]){
-                if(x==0){
-                    ans+=v[i];
-                }
-                else{
-                    while(x>=v[i] or m.find(x) != m.end()){
-                        x--;
-                    }
-                    ans+=v[i]-x;
-                    if(x) x--;
-                }
+
+        for(int i=24;i>=0;i--){
+            if(v[i]==0) break;
+            if(v[i]>=v[i+1]){
+                ans+=v[i]-max(0,v[i+1]-1);
+                v[i]=max(0,v[i+1]-1);
             }
         }
         return ans;
