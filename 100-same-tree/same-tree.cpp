5c6d2886942b108be6315b2aecb9617508f75dff
Ishan <senishan21@gmail.com>
2024-01-04 22:57:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (64.56%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -29,8 +29,12 @@ public:
     }
 
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        bool flag=true;
-        check(p,q,flag);
-        return flag;
+        if(!p and !q) return true;
+        if(!p or !q) return false;
+        if(p->val != q->val) return false;
+        return (isSameTree(p->left,q->left) and isSameTree(p->right,q->right));
+        // bool flag=true;
+        // check(p,q,flag);
+        // return flag;
     }
 };
