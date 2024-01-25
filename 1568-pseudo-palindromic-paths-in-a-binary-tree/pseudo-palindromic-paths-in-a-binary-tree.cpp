7709955d1792daa92db552542e100a049e0282ac
Ishan <senishan21@gmail.com>
2024-01-25 14:54:23 +0530

    Time: 1690 ms (5.15%) | Memory: 678.5 MB (5.14%) - LeetSync

--- 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -11,28 +11,18 @@
  */
 class Solution {
 public:
-    void util(TreeNode* root,int len,int odd,int& ans,unordered_map<int,int>& v){
-        // if(!root) return;
+    void util(TreeNode* root,int len,int odd,int& ans,unordered_map<int,int> v){
         v[root->val]++;
         if(v[root->val]%2!=0) odd++;
         else odd--;
         if(!root->left and !root->right) {
-            cout<<root->val<<":"<<odd<<" "<<v.size()<<"\n";
             if((odd==1 and len%2!=0) or odd==0) ans++;
-            if(v[root->val]==1) v.erase(root->val);
-            else v[root->val]--;
             return;
         }
         if(root->left) util(root->left,len+1,odd,ans,v);
         if(root->right) util(root->right,len+1,odd,ans,v);
-        // v[root->val]--;
-        // if(v[root->val]%2==0) odd--;
-        // else odd++;
-        if(v[root->val]==1) v.erase(root->val);
-        else v[root->val]--;
     }
     int pseudoPalindromicPaths (TreeNode* root) {
-
         unordered_map<int,int> m;
         int odd=0;
         int ans=0;
