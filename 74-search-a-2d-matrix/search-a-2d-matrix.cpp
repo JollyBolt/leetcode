b8cfef60c1ca8b533811f9024a0eaceb4a16e6a7
Ishan <senishan21@gmail.com>
2024-07-18 10:58:58 +0530

    Time: 7 ms (22.06%) | Memory: 12.2 MB (8.42%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -2,7 +2,13 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int rows = matrix.size(), cols = matrix[0].size();
+
+        //Checking the target can exist in the matrix
         if(matrix[0][0]> target or matrix[rows-1][cols-1]<target) return false;
+        //Checking if the target is 1st or last element to avaid unnecessary computation
+        if(matrix[0][0] == target or matrix[rows-1][cols-1] == target) return true;
+
+        //Binary search to find the row where the element can exist
         int start = 0, end = rows;
         while(start<end){
             int mid = start + (end - start)/2;
@@ -10,8 +16,8 @@ public:
             else start=mid+1;
         }
         int row = end - 1;
-        // if(row<0) return false;
 
+        //Binary search to find if the element exists in the row
         start = 0, end = cols - 1;
         while(start<=end){
             int mid = start + (end - start)/2;
