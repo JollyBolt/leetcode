2d559a2621e4c153f94ed1d4b1b2665b914007c4
Ishan <senishan21@gmail.com>
2024-09-04 03:01:27 +0530

    Time: 12 ms (55.67%) | Memory: 19.9 MB (26.18%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    int nextJump(int index,vector<int>& nums,vector<int>& dp){
-        if(nums[index]==0) return INT_MAX;
-        if(nums[index]>=nums.size()-index-1) return 1;
-
-        if(dp[index]!=-1){
-            return dp[index];
-        }
-
-        int jumps=INT_MAX;
-        for(int i=1;i<=nums[index];i++){
-            jumps=min(jumps,nextJump(index+i,nums,dp)==INT_MAX?INT_MAX:1+nextJump(index+i,nums,dp));
-        }
-        return dp[index]=jumps;
-    }
     int jump(vector<int>& nums){
         int n = nums.size();
         if(n==1) return 0;
-        vector<int> dp(n,-1);
-        return nextJump(0,nums,dp);
+        vector<int> hash(n,0);
+        for(int i=0;i<n;i++) hash[i]=min(n-1,i+nums[i]);
+        int jumps=1;
+        int currEnd = hash[0];
+        int maxEnd = hash[0];
+        for(int i=1;i<n;i++){
+            if(i>currEnd){
+                currEnd = maxEnd;
+                jumps++;
+            }
+            maxEnd = max(maxEnd,hash[i]);
+        }
+        return jumps;
     }
 };
-/*
-
-*/
