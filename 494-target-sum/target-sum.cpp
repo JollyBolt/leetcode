76b9abaf707460edf8e14549f0f20d5caa73f718
Ishan <senishan21@gmail.com>
2024-09-07 00:04:32 +0530

    Time: 447 ms (23.75%) | Memory: 10.9 MB (91.71%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -3,7 +3,7 @@ public:
     int util(int ind, int target, vector<int>& nums){
         if(ind==0){
             if(nums[0]==0 and target==0) return 2;
-            if(target-nums[0]==0 or target+nums[0]==0) return 1;
+            if(abs(target)==abs(nums[0])) return 1;
             return 0;
         } 
         return util(ind-1,target-nums[ind],nums)+util(ind-1,target+nums[ind],nums);
@@ -12,5 +12,8 @@ public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         return util(n-1,target,nums);
+        vector<vector<int>> dp(n+1,vector<int>(target+1));
+
+        // for(int i=)
     }
 };
