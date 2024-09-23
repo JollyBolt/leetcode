ddf3d6ee327d979aa8becab7672b7e2f0e443025
Ishan <senishan21@gmail.com>
2024-09-24 02:06:48 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (43.20%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,19 +1,9 @@
 class Solution {
 public:
-    int util(int ind, int target, vector<int>& nums){
-        if(ind==0){
-            if(target==0 and target==nums[0]) return 2;
-            if(target==0 or target==nums[0]) return 1;
-            return 0;
-        } 
-        return util(ind-1,target-nums[ind],nums)+util(ind-1,target,nums);
-    }
-
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         int sum = accumulate(nums.begin(),nums.end(),0);
         if(sum<abs(target) or (sum+target)%2!=0) return 0;
-        // return util(n-1,(sum+target)/2,nums);
         int ntarget = (sum+target)/2;
         vector<vector<int>> dp(n,vector<int>(ntarget+1,0));
         for(int i=0;i<=ntarget;i++){
@@ -30,17 +20,8 @@ public:
                 int take = 0;
                 if(nums[i]<=j) take = dp[i-1][j-nums[i]];
                 dp[i][j] = take + dp[i-1][j];
-                // if(nums[i]==0) dp[i][j] += 2;
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=ntarget;j++){
-                cout<<dp[i][j]<<" ";
-            }
-            cout<<endl;
-        }
-
         return dp[n-1][ntarget];
-
     }
 };
